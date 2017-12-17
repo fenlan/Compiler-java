c24f6df093baf358a87d103116e75397010f7029
fenlan <1475307818@qq.com>
2017-12-18 01:11:14 +0800

    程序入口

--- src/com/fenlan/compiler/main/Main.java
+++ src/com/fenlan/compiler/main/Main.java
@@ -8,22 +8,27 @@ import java.awt.*;
 public class Main extends JFrame {
 
     public static Graphics graphics;
+
+    public Main() {
+        setTitle("函数绘图语言运行结果");
+        setSize(1080, 1080);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
     public static void main(String[] args) {
 
 //        if(args.length < 2)
 //        {
 //            System.out.println("please input Source File !");		return ;
 //        }
-
-        JFrame frame=new Main();
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(1080, 1080);
-        frame.setVisible(true);
+        Main main = new Main();
     }
 
+    @Override
     public void paint(Graphics g) {
         graphics = g;
-        Parser.Parser("/home/fenlan/code/cpp/compiler/program");
+        System.out.println("执行到这里");
+        Parser.ParserAn("/home/fenlan/code/cpp/compiler/program");
     }
 
 }
--- src/com/fenlan/compiler/tree/Node.java
+++
@@ -1,6 +0,0 @@
-package com.fenlan.compiler.tree;
-
-public class Node {
-
-
-}
