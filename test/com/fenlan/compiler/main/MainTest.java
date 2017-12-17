30fcc61d6203061ef6933156b806d7a81c221fdb
fenlan <1475307818@qq.com>
2017-12-17 20:00:22 +0800

    compiler

--- test/com/fenlan/compiler/main/MainTest.java
+++ test/com/fenlan/compiler/main/MainTest.java
@@ -1,4 +1,9 @@
 package com.fenlan.compiler.main;
 
+
 public class MainTest {
+
+    public static void main(String[] args) {
+
+    }
 }
--- test/com/fenlan/compiler/main/ParserTest.java
+++ test/com/fenlan/compiler/main/ParserTest.java
@@ -1,4 +1,19 @@
 package com.fenlan.compiler.main;
 
-public class Parser {
+import com.fenlan.compiler.parser.Parser;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ParserTest extends JFrame {
+
+    public static Graphics graphics;
+    public static void main(String[] args) {
+
+//        if(args.length < 2)
+//        {
+//            System.out.println("please input Source File !");		return ;
+//        }
+        Parser.Parser("/home/fenlan/code/cpp/compiler/program");
+    }
 }
