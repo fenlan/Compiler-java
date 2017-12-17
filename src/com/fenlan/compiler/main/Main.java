5a95e98f58524a73b8a9a3d3df128b6be81b6378
fenlan <1475307818@qq.com>
2017-12-17 19:58:26 +0800

    主函数

--- src/com/fenlan/compiler/main/Main.java
+++ src/com/fenlan/compiler/main/Main.java
@@ -1,8 +1,29 @@
 package com.fenlan.compiler.main;
 
-public class Main {
+import com.fenlan.compiler.parser.Parser;
 
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+    public static Graphics graphics;
     public static void main(String[] args) {
 
+//        if(args.length < 2)
+//        {
+//            System.out.println("please input Source File !");		return ;
+//        }
+
+        JFrame frame=new Main();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(1080, 1080);
+        frame.setVisible(true);
     }
+
+    public void paint(Graphics g) {
+        graphics = g;
+        Parser.Parser("/home/fenlan/code/cpp/compiler/program");
+    }
+
 }
