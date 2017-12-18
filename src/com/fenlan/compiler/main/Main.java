b33cbf28d64e1180e3f68795b74f6bd51bb5227e
fenlan <1475307818@qq.com>
2017-12-18 21:41:23 +0800

    程序入口

--- src/com/fenlan/compiler/main/Main.java
+++ src/com/fenlan/compiler/main/Main.java
@@ -11,24 +11,27 @@ public class Main extends JFrame {
 
     public Main() {
         setTitle("函数绘图语言运行结果");
-        setSize(1080, 1080);
+        setSize(720, 480);
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
     public static void main(String[] args) {
-
-//        if(args.length < 2)
-//        {
-//            System.out.println("please input Source File !");		return ;
-//        }
         Main main = new Main();
+        Parser.ParserAn("src/com/fenlan/compiler/program/program1");
     }
 
     @Override
     public void paint(Graphics g) {
         graphics = g;
-        System.out.println("执行到这里");
-        Parser.ParserAn("/home/fenlan/code/cpp/compiler/program");
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        for (double[] tuple : Parser.list) {
+            graphics.drawOval((int)tuple[0], (int)tuple[1], 2, 2);
+            System.out.println("(" + tuple[0] + "," + tuple[1] + ")");
+        }
     }
 
 }
